c05b40ec4f4129b50788931c5cbb8a4ed6a6a2ef
Mark Bates <mark@markbates.com>
2015-07-27 18:01:27 -0400

    Initial commit

---
+++ lib/has_states.rb
@@ -0,0 +1,34 @@
+require "has_states/version"
+
+module HasStates
+
+  def self.included(klass)
+    klass.extend(ClassMethods)
+  end
+
+  module ClassMethods
+
+    def has_states(*states, **options)
+      options = {attr_name: "state", const_name: "states"}.merge(options)
+      states.flatten!
+      self.const_set(options[:const_name].upcase, states.map{|s| s.to_s})
+
+      states.each do |s|
+        s = s.to_s
+        attr = options[:attr_name].to_sym
+        define_method("#{s}?") do
+          return self.send(attr).to_s == s
+        end
+        define_method("#{s}!") do
+          self.send("#{attr}=", s)
+          self.save!
+        end
+        self.class.send("define_method", s) do
+          where(attr => s)
+        end
+      end
+    end
+
+  end
+
+end
---
+++ test/lib/has_states_test.rb
@@ -0,0 +1,65 @@
+require 'test_helper'
+
+class Foo
+  include HasStates
+
+  has_states %w{completed running}
+
+  attr_accessor :state
+end
+
+class Bar
+  include HasStates
+
+  has_states :started, :stopped, attr_name: "status", const_name: "statuses"
+
+  attr_accessor :status
+end
+
+describe HasStates do
+
+  it "creates a list at the class level" do
+    Foo::STATES.must_equal %w{completed running}
+    Bar::STATUSES.must_equal %w{started stopped}
+  end
+
+  it "creates ? methods" do
+    f = Foo.new
+
+    f.state = "completed"
+    f.completed?.must_equal true
+    f.running?.must_equal false
+
+    f.state = "running"
+    f.completed?.must_equal false
+    f.running?.must_equal true
+
+    b = Bar.new
+
+    b.status = "started"
+    b.started?.must_equal true
+    b.stopped?.must_equal false
+
+    b.status = "stopped"
+    b.started?.must_equal false
+    b.stopped?.must_equal true
+  end
+
+  it "creates ! methods" do
+    f = Foo.new
+    f.expects(:save!)
+
+    f.state = "completed"
+    f.running!
+    f.state.must_equal "running"
+  end
+
+  it "creates where methods" do
+    Foo.expects(:where).with(state: "completed")
+    Foo.completed
+
+    Bar.expects(:where).with(status: "started")
+    Bar.started
+  end
+
+end
